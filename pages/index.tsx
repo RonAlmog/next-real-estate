eaa8afdcda180500773e44404f9af52bb8321a02
Ron Almog <ron8132@gmail.com>
2022-10-27 19:08:35 -0400

    add mapbox

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,16 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 import Layout from "../src/components/layout";
+import Map from "src/components/map";
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <div className="flex">
+      <div className="w-1/2 pb-4" style={{ maxHeight: "calc(100vh-64px)" }}>
+        house list
+      </div>
+      <div className="w-1/2">
+        <Map />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/map.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { useRef, useState } from "react";
+import Link from "next/link";
+import Map, {
+  Marker,
+  Popup,
+  NavigationControl,
+  FullscreenControl,
+  ScaleControl,
+  GeolocateControl,
+  ViewState,
+} from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import Pin from "./pin";
+import { map } from "@firebase/util";
+
+interface IProps {}
+const token = process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN;
+
+const MapComp = (props: IProps) => {
+  const mapRef = useRef<Map | null>(null);
+  const [viewport, setViewport] = useState<ViewState>({
+    latitude: 44,
+    longitude: -79,
+    zoom: 10,
+    bearing: 0,
+    pitch: 0,
+    padding: { top: 5, bottom: 5, left: 5, right: 5 },
+  });
+  return (
+    <div className="text-black relative" id="map">
+      <Map
+        initialViewState={{
+          latitude: 44,
+          longitude: -79,
+          zoom: 10,
+          bearing: 0,
+          pitch: 0,
+        }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxAccessToken={token}
+      >
+        <Marker longitude={-122.4} latitude={37.8} color="green">
+          <Pin />
+        </Marker>
+        <NavigationControl position="bottom-right" />
+        <FullscreenControl />
+        <GeolocateControl />
+        <ScaleControl />
+      </Map>
+    </div>
+  );
+};
+
+export default MapComp;
