7cada0e3055dc63054ce9a3a2ce16453662d2de2
Ron Almog <ron8132@gmail.com>
2022-10-29 10:32:54 -0400

    upload and preview image

--- src/components/houseForm.tsx
+++ src/components/houseForm.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import { useState, useEffect, ChangeEvent } from "react";
 import { useForm } from "react-hook-form";
@@ -15,6 +16,7 @@ interface IFormData {
 
 const HouseForm = ({}: Props) => {
   const [submitting, setSubmitting] = useState(false);
+  const [previewImage, setPreviewImage] = useState<string>();
   const {
     register,
     handleSubmit,
@@ -50,6 +52,65 @@ const HouseForm = ({}: Props) => {
         />
         {errors.address && <p>{errors.address.message}</p>}
         <h2>{address}</h2>
+        <div className="mt-4">
+          <label
+            htmlFor="image"
+            className="p-4 border-dashed border-4 border-gary-600 block cursor-pointer"
+          >
+            Click to add image (16:9)
+          </label>
+          <input
+            type="file"
+            id="image"
+            accept="image/*"
+            style={{ display: "none" }}
+            {...register("image", { required: true })}
+            onChange={(event: ChangeEvent<HTMLInputElement>) => {
+              if (event.target.files?.length) {
+                const file = event.target.files[0];
+                console.log("event target files:", file);
+                const reader = new FileReader();
+                reader.onloadend = () => {
+                  console.log(reader.result);
+                  setPreviewImage(reader.result as string);
+                };
+                reader.readAsDataURL(file);
+              }
+            }}
+          />
+          {previewImage && (
+            <img src={previewImage} className="mt-4 object-cover" />
+          )}
+          {errors.image && <p>{errors.image.message}</p>}
+        </div>
+      </div>
+      <div className="mt-4">
+        <label htmlFor="bedrooms" className="block">
+          Bedrooms
+        </label>
+        <input
+          type="number"
+          id="bedrooms"
+          className="p2"
+          {...register("bedrooms", {
+            required: "Please enter number of bedrooms",
+            max: { value: 10, message: "Thats too big!" },
+            min: { value: 1, message: "Must have at least one bedroom" },
+          })}
+        />
+        {errors.bedrooms && <span>{errors.bedrooms.message}</span>}
+      </div>
+      <div className="mt-4">
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded"
+          disabled={submitting}
+        >
+          Save
+        </button>
+        <Link href="/" className="ml-3">
+          Cancel
+        </Link>
       </div>
     </form>
   );
