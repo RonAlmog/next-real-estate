941bb3094ff8a5493bc6fea921d83362ba676ae4
Ron Almog <ron8132@gmail.com>
2022-10-29 22:43:27 -0400

    show form only if address selected

--- src/components/houseForm.tsx
+++ src/components/houseForm.tsx
@@ -25,10 +25,6 @@ const HouseForm = ({}: Props) => {
     formState: { errors },
   } = useForm<IFormData>({ defaultValues: {} });
   const address = watch("address");
-  //   useEffect(() => {
-  //     register({ name: "address" }, { required: "Please enter the address" });
-  //     register({ name: "latitude" }, { required: true, min: -90, max: 90 });
-  //   }, [register]);
 
   const onSubmit = (data: IFormData) => {
     setSubmitting(true);
@@ -42,6 +38,7 @@ const HouseForm = ({}: Props) => {
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="mx-auto max-w-xl py-4">
       <h1 className="text-xl">Add a New House</h1>
+
       <div className="mt-4">
         <label htmlFor="search" className="block">
           Search for your address
@@ -56,67 +53,73 @@ const HouseForm = ({}: Props) => {
           defaultValue=""
         />
         {errors.address && <p>{errors.address.message}</p>}
-        <h2>{address}</h2>
-        <div className="mt-4">
-          <label
-            htmlFor="image"
-            className="p-4 border-dashed border-4 border-gary-600 block cursor-pointer"
-          >
-            Click to add image (16:9)
-          </label>
-          <input
-            type="file"
-            id="image"
-            accept="image/*"
-            style={{ display: "none" }}
-            {...register("image", { required: "Please select an image file" })}
-            onChange={(event: ChangeEvent<HTMLInputElement>) => {
-              if (event.target.files?.length) {
-                const file = event.target.files[0];
-                console.log("event target files:", file);
-                const reader = new FileReader();
-                reader.onloadend = () => {
-                  console.log(reader.result);
-                  setPreviewImage(reader.result as string);
-                };
-                reader.readAsDataURL(file);
-              }
-            }}
-          />
-          {previewImage && (
-            <img src={previewImage} className="mt-4 object-cover" />
-          )}
-          {errors.image && <p>{errors.image.message}</p>}
-        </div>
       </div>
-      <div className="mt-4">
-        <label htmlFor="bedrooms" className="block">
-          Bedrooms
-        </label>
-        <input
-          type="number"
-          id="bedrooms"
-          className="p2"
-          {...register("bedrooms", {
-            required: "Please enter number of bedrooms",
-            max: { value: 10, message: "Thats too big!" },
-            min: { value: 1, message: "Must have at least one bedroom" },
-          })}
-        />
-        {errors.bedrooms && <span>{errors.bedrooms.message}</span>}
-      </div>
-      <div className="mt-4">
-        <button
-          type="submit"
-          className="bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded"
-          disabled={submitting}
-        >
-          Save
-        </button>
-        <Link href="/" className="ml-3">
-          Cancel
-        </Link>
-      </div>
+      {address && (
+        <>
+          <div className="mt-4">
+            <label
+              htmlFor="image"
+              className="p-4 border-dashed border-4 border-gary-600 block cursor-pointer"
+            >
+              Click to add image (16:9)
+            </label>
+            <input
+              type="file"
+              id="image"
+              accept="image/*"
+              style={{ display: "none" }}
+              {...register("image", {
+                required: "Please select an image file",
+              })}
+              onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                if (event.target.files?.length) {
+                  const file = event.target.files[0];
+                  console.log("event target files:", file);
+                  const reader = new FileReader();
+                  reader.onloadend = () => {
+                    console.log(reader.result);
+                    setPreviewImage(reader.result as string);
+                  };
+                  reader.readAsDataURL(file);
+                }
+              }}
+            />
+            {previewImage && (
+              <img src={previewImage} className="mt-4 object-cover" />
+            )}
+            {errors.image && <p>{errors.image.message}</p>}
+          </div>
+
+          <div className="mt-4">
+            <label htmlFor="bedrooms" className="block">
+              Bedrooms
+            </label>
+            <input
+              type="number"
+              id="bedrooms"
+              className="p2"
+              {...register("bedrooms", {
+                required: "Please enter number of bedrooms",
+                max: { value: 10, message: "Thats too big!" },
+                min: { value: 1, message: "Must have at least one bedroom" },
+              })}
+            />
+            {errors.bedrooms && <span>{errors.bedrooms.message}</span>}
+          </div>
+          <div className="mt-4">
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded"
+              disabled={submitting}
+            >
+              Save
+            </button>
+            <Link href="/" className="ml-3">
+              Cancel
+            </Link>
+          </div>
+        </>
+      )}
     </form>
   );
 };
